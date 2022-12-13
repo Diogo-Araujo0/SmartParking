ca2d75e4117eb28ffff1a1d99151ad70ce4469bb
Pedro Leiras <pedromdleiras@gmail.com>
2022-12-13 14:28:15 +0000

    add registo

---
+++ app/src/main/java/ipca/djpm/smartparking/UserRegisterActivity.kt
@@ -0,0 +1,11 @@
+package ipca.djpm.smartparking
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class UserRegisterActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_user_register)
+    }
+}
