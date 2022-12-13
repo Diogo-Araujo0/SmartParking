640011a0f34354f69afad77c2a70098247f5fcf5
Pedro Leiras <pedromdleiras@gmail.com>
2022-12-13 16:41:10 +0000

    update login design

--- app/src/main/java/ipca/djpm/smartparking/MainActivity.kt
+++ app/src/main/java/ipca/djpm/smartparking/MainActivity.kt
@@ -1,11 +1,20 @@
 package ipca.djpm.smartparking
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseAdapter
+import android.widget.ImageView
+import android.widget.ListView
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_user_login)
     }
 }
