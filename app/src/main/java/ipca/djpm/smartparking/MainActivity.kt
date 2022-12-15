c0fd63465b8aa6984e06d27bd2ebf376bb50f83b
Pedro Leiras <pedromdleiras@gmail.com>
2022-12-15 12:19:56 +0000

    update colors and themes

--- app/src/main/java/ipca/djpm/smartparking/MainActivity.kt
+++ app/src/main/java/ipca/djpm/smartparking/MainActivity.kt
@@ -42,6 +42,5 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_register)
-
     }
 }
