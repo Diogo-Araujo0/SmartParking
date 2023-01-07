5951753cdf57bfcb6f3b856375a6f04e24813846
Pedro Leiras <pedromdleiras@gmail.com>
2023-01-07 22:24:18 +0000

    add firebase

--- app/src/main/java/ipca/djpm/smartparking/LoginActivity.kt
+++ app/src/main/java/ipca/djpm/smartparking/LoginActivity.kt
@@ -1,11 +1,34 @@
 package ipca.djpm.smartparking
 
+import android.app.Activity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
 
 class LoginActivity: AppCompatActivity() {
+    var resultLauncher : ActivityResultLauncher<Intent>? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_login)
+        resultLauncher = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){
+
+        }
+
+        val buttonLogin = findViewById<Button>(R.id.buttonLogin)
+        val buttonNewUser = findViewById<Button>(R.id.buttonNewUser)
+
+        buttonLogin.setOnClickListener{
+            val intent = Intent(this@LoginActivity, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        buttonNewUser.setOnClickListener{
+            resultLauncher?.launch(Intent(this@LoginActivity, UserRegisterActivity::class.java))
+        }
     }
 }
--- app/src/main/java/ipca/djpm/smartparking/UserRegisterActivity.kt
+++ app/src/main/java/ipca/djpm/smartparking/UserRegisterActivity.kt
@@ -4,11 +4,27 @@ import android.content.Intent
 import android.os.Bundle
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 class UserRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_user_register)
+
+        val buttonRegister = findViewById<Button>(R.id.buttonRegister)
+        val buttonExistingUser = findViewById<Button>(R.id.buttonExistingUser)
+
+        buttonRegister.setOnClickListener{
+            val intent = Intent(this@UserRegisterActivity, LoginActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+        buttonExistingUser.setOnClickListener{
+            val intent = Intent()
+            setResult(RESULT_OK, intent)
+            finish()
+        }
     }
 }
