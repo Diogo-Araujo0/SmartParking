98b3834f2171efce4af84ec0284ee2a02c044c89
Pedro Leiras <pedromdleiras@gmail.com>
2023-01-10 20:37:23 +0000

    Update MainActivity.kt

--- app/src/main/java/ipca/djpm/smartparking/MainActivity.kt
+++ app/src/main/java/ipca/djpm/smartparking/MainActivity.kt
@@ -52,7 +52,7 @@ class MainActivity : AppCompatActivity() {
         val navView: BottomNavigationView = binding.navView
 
         val navController = findNavController(R.id.nav_host_fragment_activity_main)
-        // Passing each menu ID as a set of Ids because each
+        // Passing each menu ID as a set of Ids because eac
         // menu should be considered as top level destinations.
         val appBarConfiguration = AppBarConfiguration(
             setOf(
